3d143f8dff0990d639ec9eb0963d3f13c4c30517
saifyedes <saifyedes20@gmail.com>
2025-12-12 22:25:55 +0100

    changement d'api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://roomtn.onrender.com';
 
 export const API_BASE_URL = `${API_URL}/api`;
 export const UPLOADS_URL = `${API_URL}/uploads`;
